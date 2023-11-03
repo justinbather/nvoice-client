55f69a921861d6722d6df1c3bcb59701afaa9cba
justinbather <justinbather@hotmail.com>
2023-11-03 12:56:23 -0400

    path fixes

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import LandingPage from "./pages/LandingPage/LandingPage";
 import CompanyLogin from "./components/CompanyLogin/CompanyLogin";
 import UserLogin from "./components/UserLogin/UserLogin";
 import LandingHeader from "./components/LandingHeader/LandingHeader";
-import CompanyHomePage from "./pages/CompanyHome/CompanyHomePage";
-import UserHomePage from "./pages/UserHome/UserHomePage";
+// import CompanyHomePage from "./pages/CompanyHome/CompanyHomePage";
+// import UserHomePage from "./pages/UserHome/UserHomePage";
 import { CompanyInvoicePage } from "./pages/CompanyInvoicePage/CompanyInvoicePage";
 
 function App() {
@@ -17,8 +17,8 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/login/company" element={<CompanyLogin />} />
           <Route path="/login/user" element={<UserLogin />} />
-          <Route path="/company/home" element={<CompanyHomePage />} />
-          <Route path="/user/home" element={<UserHomePage />} />
+          {/* <Route path="/company/home" element={<CompanyHomePage />} /> */}
+          {/* <Route path="/user/home" element={<UserHomePage />} /> */}
           <Route
             path="/company/invoices/:invoiceId"
             element={<CompanyInvoicePage />}
